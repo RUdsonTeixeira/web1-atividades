ae94a6923332995061401381cb98978a532cae0d
Rudoson Da Silva Teixeira <rudson.teixeira@estudante.ifms.edu.br>
2021-05-24 15:31:27 -0300

    AtvAvaliativaCombustivel

---
+++ atividadeAvaliativa/app-viabilidade-combustivel/app/js/app.js
@@ -0,0 +1,37 @@
+const inputAlcool = document.querySelector("input[name = valorDoAlcool]")
+const inputGasolina = document.querySelector("input[name = valorDaGasolina]")
+const btnCalcular = form.querySelector("button[name=btnCalcular]")
+const divResultado = document.querySelector('.resultado')
+function calcular(){
+    const valorAlcool = inputAlcool
+    const valorGasolina = inputGasolina
+    const resultado
+    if ((valorAlcool) <= ((valorGasolina*70)/100)) {
+        let porcentagem 
+        porcentagem = ((valorAlcool-valorGasolina)/(valorAlcool/100))
+         resultado =`
+            <p>
+                O valor do  Alcool é ${porcentagem} do valor da Gasolina,<br>
+                ou seja, o combustível mais vantajoso é:
+            </p>
+            <p>
+                <strong>Álcool</Strong>
+            </p>
+        `    
+    } else {
+         resultado =`
+            <p>
+                O valor do  Alcool é ${porcentagem} do valor da Gasolina,<br>
+                ou seja, o combustível mais vantajoso é:
+            </p>
+            <p>
+                <strong>Gasolina</Strong>
+            </p>
+        `
+        
+    }
+    divResultado.innerHTML = resultado.value    
+    
+}
+
+btnCalcular.addEventListener('click',calcular)
